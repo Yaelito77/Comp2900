fde4d45383c755aef08faf654d877463ae1439d5
Yaelito77 <Yaelfabian007@gmail.com>
2024-02-06 14:24:14 -0400

    update my files

--- Modulo-01/Lab-01/app.py
+++ Modulo-01/Lab-01/app.py
@@ -1,4 +1,9 @@
-print("Autor: Yael Maldonado Diaz")
-mensaje = "Esto es un mensaje de prueba!!"
-
+print("Hola!")
+autor = "Yael Maldonado Diaz"
+mensaje = "Esto es un mensaje de prueba!!" #---- string value
+gpa = 3.63 #------------------------------------ number (real) value
+isValid = True #-------------------------------- boolean value
 print(mensaje)
+print(f"El autor es {autor}")
+print(f"El GPA es: {gpa}")
+print(isValid)
