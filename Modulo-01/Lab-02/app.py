cf5082f442406fa8b578f3d0a5918c1fa94a0e3b
Yaelito77 <Yaelfabian007@gmail.com>
2024-02-08 13:50:21 -0400

    update my files

--- Modulo-01/Lab-02/app.py
+++ Modulo-01/Lab-02/app.py
@@ -1,5 +1,7 @@
 print("Hola Mundo!")
 
 nombre = input("Entre su nombre: ")
+edad = input("Entre su edad: ")
 gpa = float(input("Entre su GPA: "))
-print(f"Hola {nombre}!!")
+
+print(f"Hola {nombre}!! Usted tiene {edad} años y su GPA es {gpa}")
