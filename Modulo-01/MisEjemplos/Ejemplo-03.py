4d1b7a7fd07f43ade21312db2fd80d0c3364ab75
Yaelito77 <Yaelfabian007@gmail.com>
2024-02-22 14:23:06 -0400

    Update file

--- Modulo-01/MisEjemplos/Ejemplo-03.py
+++ Modulo-01/MisEjemplos/Ejemplo-03.py
@@ -3,4 +3,22 @@ def suma(a, b):
 
 resultado = suma(5, 4) #-- Llamar a la funcion y recibir el resultado
 
-print(resultado) #-------- Imprimir el resultado 
+print(F"La suma es de: {resultado}") #-------- Imprimir el resultado 
+print("")
+
+#-----------------------------------------------------------------------------
+
+def resta(a ,b):
+    return (a - b)
+
+print("Para restar...")
+print("")
+
+primerNum = int(input("Entre el primer numero: "))
+
+segundonum = int(input("Entre el segundo numero: "))
+
+respuesta = resta(primerNum, segundonum)
+print("")
+print(f"La resta de los numeros es: {respuesta}")
+print("")
