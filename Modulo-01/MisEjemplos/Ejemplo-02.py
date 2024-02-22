784e7352759005e9e4f5b09741d2f7ca4e09c39d
Yaelito77 <Yaelfabian007@gmail.com>
2024-02-22 14:01:55 -0400

    Update file

--- Modulo-01/MisEjemplos/Ejemplo-02.py
+++ Modulo-01/MisEjemplos/Ejemplo-02.py
@@ -1,4 +1,4 @@
-def miFuncion ():
+def miFuncion():
     print ("Hola como estas? ")
 
 miFuncion()
@@ -10,3 +10,17 @@ def saludo(mensaje):
     print(f"Hace tiempo, {mensaje}!")
 
 saludo ("No se de ti")
+
+
+def pedirNombre():
+    nombre = input("diga su nombre: ")
+    
+def pedirGpa():
+    gpa = float(input("Diga su GPA: "))
+
+print("Bienvenido")
+pedirNombre()
+pedirGpa()
+
+print(f"Su nombre es {nombre} y su GPA es de {gpa}")
+
---
+++ Modulo-01/MisEjemplos/Ejemplo-03.py
@@ -0,0 +1,6 @@
+def suma(a, b):
+    return (a + b) #------ Retorna ;a suma de a + b
+
+resultado = suma(5, 4) #-- Llamar a la funcion y recibir el resultado
+
+print(resultado) #-------- Imprimir el resultado 
