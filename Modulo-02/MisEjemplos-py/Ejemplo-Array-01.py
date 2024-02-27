02c2664ea2cbd9d8a0bde11853a0a01218669a32
Yaelito77 <Yaelfabian007@gmail.com>
2024-02-27 14:25:52 -0400

    Update file

---
+++ Modulo-02/MisEjemplos-py/Ejemplo-Array-01.py
@@ -0,0 +1,11 @@
+cars = ['Ford', 'Volvo', 'BMW']
+
+print(cars[0])
+print("")
+
+import array
+years = array.array('i', [1971, 2005, 2010, 2020])
+
+print(years)
+print(f"El primer elemento es: {years[0]}") # primer elemento
+print(f"El ultimo elemento es: {years[3]}") # ultimo elemento
