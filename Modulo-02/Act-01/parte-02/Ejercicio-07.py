add1a60f1b0c3eb47fe8ef3e3ed120d16b115d44
Yaelito77 <Yaelfabian007@gmail.com>
2024-03-07 11:19:05 -0400

    Update file

---
+++ Modulo-02/Act-01/parte-02/Ejercicio-07.py
@@ -0,0 +1,10 @@
+array = [43, 75, 12, 55, 15, 27]
+
+total_add = 0
+
+for element in array:
+    total_add += element
+
+average = total_add / len(array)
+
+print(f"El promedio de los elementos del arreglo es: {average}")
