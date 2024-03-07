99622fc3bac7b7e57a949b43166c731cef0ddd54
Yaelito77 <Yaelfabian007@gmail.com>
2024-03-06 21:01:46 -0400

    Update file

--- Modulo-02/Act-01/Ejercicio-06.py
+++ Modulo-02/Act-01/Ejercicio-06.py
@@ -1,4 +1,8 @@
 def invertir_cadena(cadena):
     return(cadena[::-1])
 
+cadena = "Estamos aqui tranquilos!"
+cadena_invertida = invertir_cadena(cadena)
 
+print(f"Texto normal: {cadena}")
+print(f"Texto invertido: {cadena_invertida}")
