2b538528052ef19508ea142b3afb119363c9150c
Yaelito77 <Yaelfabian007@gmail.com>
2024-03-07 13:38:43 -0400

    Update file

--- Modulo-02/MisEjemplos-py/Ejemplo-Array-04.py
+++ Modulo-02/MisEjemplos-py/Ejemplo-Array-04.py
@@ -1,11 +1,13 @@
-def promedio(lista_numero)
+def Average(lst):
+    return sum(lst) / len(lst)
+
+def promedio(lista_numero):
     suma = 0
     array_len = 0
     for valor in lista_numero:
         suma = suma + valor
         array_len = array_len + 1
     return (suma / array_len)
-
 
 lst = [15, 65, 76, 23, 16, 34, 72, 12]
 average = Average(lst)
