79a05d1808fa014ee63de1bc8883e33a9ddcebeb
Yaelito77 <Yaelfabian007@gmail.com>
2024-03-12 13:56:31 -0400

    Update file

---
+++ Modulo-02/Act-01/parte-02/Ejercicio-03-bien.py
@@ -0,0 +1,9 @@
+def invertir_arreglo(arreglo):
+    return arreglo[::-1]
+
+arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+arreglo_invertido = invertir_arreglo(arreglo)
+
+print(f"El arreglo original es: {arreglo}")
+print(f"El arreglo invertido es: {arreglo_invertido}")
