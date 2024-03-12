06c66102d71c29badc5cdabb975193716bba8205
Yaelito77 <Yaelfabian007@gmail.com>
2024-03-12 13:49:46 -0400

    Update file

---
+++ Modulo-02/Act-01/parte-02/Ejercicio-02-bien.py
@@ -0,0 +1,11 @@
+def encontrar_maximo(arreglo):
+    maximo = arreglo[0]
+    for elemento in arreglo:
+        if elemento > maximo:
+            maximo = elemento
+    return maximo
+
+arreglo = [12, 43, 17, 9, 8]
+maximo_elemento = encontrar_maximo(arreglo)\
+
+print(f"El maximo elemento en el arreglo es: {maximo_elemento}")
