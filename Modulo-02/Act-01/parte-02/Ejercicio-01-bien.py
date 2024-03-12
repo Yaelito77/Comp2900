d4478aa8fd1accdd3333e0168d181d02b3d1f91f
Yaelito77 <Yaelfabian007@gmail.com>
2024-03-12 13:37:09 -0400

    Update file

---
+++ Modulo-02/Act-01/parte-02/Ejercicio-01-bien.py
@@ -0,0 +1,11 @@
+def sumar_arreglo(arreglo):
+    suma = 0
+    for elemento in arreglo:
+        suma += elemento
+    return suma
+def main():
+    arreglo = [1, 2, 3, 4,  5]
+
+    suma_total = sumar_arreglo(arreglo)
+
+    print(F"La suma total de los elementos del arreglo es: {suma_total}")
