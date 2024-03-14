6c83a490bc2ff6002e31ce6b7b12d37eb14402e7
Yaelito77 <Yaelfabian007@gmail.com>
2024-03-14 13:53:27 -0400

    Update file

--- Modulo-03/Leccion-01/Prueba-05.py
+++ Modulo-03/Leccion-01/Prueba-05.py
@@ -4,4 +4,4 @@ def buscar_elemento(arreglo, elemento):
 # Prueba
 arreglo = [1, 2, 3, 4, 5]
 assert buscar_elemento(arreglo, 3), "El elemento no fueencontrado cuando debería."
-assert not buscar_elemento(arreglo, 6), "El elemento fueencontrado cuando no debería."
+assert buscar_elemento(arreglo, 6), "El elemento fueencontrado cuando no debería."
