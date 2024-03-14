4f6aeb3c875479df99f74803add9dcc23165f9b9
Yaelito77 <Yaelfabian007@gmail.com>
2024-03-14 15:29:04 -0400

    Update file

--- Modulo-02/Act-01/parte-02/Ejercicio-05-bien.py
+++ Modulo-02/Act-01/parte-02/Ejercicio-05-bien.py
@@ -1,15 +1,15 @@
 def eliminar_elemento(arreglo, elemento):
-
     if elemento in arreglo:
-        arreglo.remove(elemento)
-        return arreglo
+        arreglo_modificado = arreglo.copy()  # Crear una copia del arreglo original
+        arreglo_modificado.remove(elemento)
+        return arreglo_modificado
     else:
         print("El elemento no se encuentra en el arreglo.")
         return arreglo
 
 numeros = [2, 8, 13, 23, 31]
 
-elemento_a_eliminar = 2
+elemento_a_eliminar = 8
 arreglo_modificado = eliminar_elemento(numeros, elemento_a_eliminar)
 
 print("Arreglo original:", numeros)
