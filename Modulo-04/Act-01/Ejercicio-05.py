27ba7caf0a5f4ead6518af2cc2cb7d510d157a51
Yaelito77 <Yaelfabian007@gmail.com>
2024-04-02 22:01:44 -0400

    Update file

--- Modulo-04/Act-01/Ejercicio-05.py
+++ Modulo-04/Act-01/Ejercicio-05.py
@@ -8,4 +8,4 @@ valores = [(7, 7), (9, 3), (12, 4), (7, 3)]
 
 for n, k in valores:
     coeficiente = coeficiente_binomial(n, k)
-    print(f"El coeficiente binominal de {n} tomado {k} a la vez es: {coeficiente}")
+    print(f"El coeficiente binomial de {n} tomado {k} a la vez es: {coeficiente}")
